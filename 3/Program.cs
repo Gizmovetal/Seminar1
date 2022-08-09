20e2c36ecabe5f6ae73450af50626d92abc47bc0
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-09 12:51:54 +0300

    Homework number 3

---
+++ 3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число");
+string num = Console.ReadLine() ?? "";
+int num_i;
+
+if (int.TryParse(num, out num_i))
+{
+    if (num_i % 2 == 0) 
+    {
+        Console.Write("Число ");
+        Console.Write(num_i);
+        Console.Write(" чётное");
+    }   
+    else 
+    {
+        Console.Write("Число ");
+        Console.Write(num_i);
+        Console.Write(" нечётное");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка при вводе данных, введите число!");
+}
+
