d2de1ad5174f92eb66ee5e14d7cbd101da8a1971
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-09 13:01:25 +0300

    Homework number 4

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число N:");
+string num = Console.ReadLine() ?? "";
+int num_i;
+int count = 1;
+
+if (int.TryParse(num, out num_i))
+{   
+    Console.WriteLine("Все чётные числа от 1 до N: ");
+    while (count<=num_i)
+    {
+    if (count % 2 == 0) Console.WriteLine(count);
+    count++; 
+    }   
+}
+else
+{
+    Console.WriteLine("Ошибка при вводе данных, введите число!");
+}
+
