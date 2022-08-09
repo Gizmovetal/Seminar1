5f8896ad976acfff47edd14b3383680dfb966d3e
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-09 12:03:10 +0300

    Homework number 1

---
+++ 1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите первое число");
+string number_1 = Console.ReadLine() ?? "";
+int number_1i;
+Console.WriteLine("Введите второе число");
+string number_2 = Console.ReadLine() ?? "";
+int number_2i;
+
+if (int.TryParse(number_1, out number_1i))
+{
+    if (int.TryParse(number_2, out number_2i))
+    {
+        if (number_1i > number_2i)
+        {
+            Console.Write("Максимальное число = ");
+            Console.Write(number_1i);
+        }
+        else
+        {
+            Console.Write("Максимальное число = ");
+            Console.Write(number_2i);
+        }
+    }
+        else
+    {
+        Console.WriteLine("Ошибка, введите число!");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка, введите число!");
+}
